0eedeb77752b160739c48e8d63f0c17eaa663e49
kerimkuscu <Kerim>
2023-11-04 00:53:21 +0300

    first commit

---
+++ styles/layoutStyles.js
@@ -0,0 +1,24 @@
+const mainStyles = {
+    view: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        paddingTop: 50,
+        paddingHorizontal: 20
+    },
+
+    text: {
+        textAlign: 'center',
+        fontWeight: 'bold'
+    },
+
+    pt30: {
+        paddingTop: 30
+    },
+
+    pb30: {
+        paddingBottom: 30
+    },
+};
+
+export default mainStyles;
